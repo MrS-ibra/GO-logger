02882fb5b6d67c81f5d39fcbf13003686cdd67b1
MrS-ibra <ibraabuarab24@gmail.com>
2025-08-26 05:23:18 +0300

    Update SendChart.ps1

--- SendChart.ps1
+++ SendChart.ps1
@@ -92,7 +92,7 @@ $chartConfig = @{
     }
 
     # Overlay logo on chart
-    & $magickPath $ChartPath $LogoPath -geometry 260x260+10+10 -composite $ChartPath
+    & $magickPath $ChartPath $LogoPath -gravity north -geometry 260x260+10+10 -composite $ChartPath
 
     # Send to Discord
     Invoke-RestMethod -Uri $WebhookUrl -Method Post -Form @{
